9915362f74a57f65cd54ee6fbc85e25c478dd0ce
dotmaster <greg@synaptic-labs.net>
2012-09-02 15:03:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+Expand Selection to Line Before
+=========================================
+
+A command to expand the current selection to the line before, which is something I need quite often.
+
+Each time this command runs it will expand one line above.
+
+After expanding, you can also contract the selection back by one line.
+
+Usage
+-----
+
+Both commands are available in the `Selection` menu and bound to the following keyboard shortcuts on Windows and Linux:
+
+    Expand Selection by one line                Alt+Shift+Up
+    Contract by one line                        Alt+Shift+Down
+
+Or similarly, on OS X:
+
+    Expand Selection by one line                ⌥⇧↑ (Option+Ushift+Up)
+    Contract by one line                        ⌥⇧↓ (Option+Shift+Down)
---
+++ expand_selection_to_line_before.py
@@ -0,0 +1,138 @@
+import sublime_plugin
+import sublime
+
+
+class ExpandSelectionToLineBefore(sublime_plugin.TextCommand):
+    """Expands the current selection region by one line. On first call if the cursor is not at the beginning of the line, the current line will be selected.
+
+    Consecutive calls continue to expand the selection by another line.
+
+    """
+
+    def find_previous(self, from_point, to_point):
+        """Walks backwards from a point, until it finds a region that matches
+        the given regex.
+
+        Attempts to find the longest match possible. So while "oobar:
+        function()" may be a match, this will continue walking backwards until
+        it can no longer match, since "foobar: function()" is preferred.
+
+        """
+
+        #find the current line number
+        (line, col) = self.view.rowcol(from_point)
+        end_point = self.view.line(to_point).end()
+
+        #find the point that corresponds to the beginnning of the line
+        line_point = self.view.text_point(line, 0)
+
+        #return if we reached the beginning
+        if not line_point > -1:
+            return None
+
+        if line_point == from_point:
+            #if we are at the beginning of the line the line has been selected already and we select theline before
+            start_point = self.view.text_point(line - 1, 0)
+        else:
+            start_point = line_point
+
+        return start_point, end_point
+
+    def expand_by_line(self, original_region):
+        """Returns a new, expanded region to cover the closest function that encloses the given region.
+
+        If there is no enclosing region, the original region is returned.
+
+        """
+        search_back_from_point = original_region.begin()
+        end_point = original_region.end()
+
+        line_start_point, end_point = self.find_previous(search_back_from_point, end_point)
+
+        total_region = sublime.Region(line_start_point, end_point)
+        #scroll the view to show the selection
+        self.view.show(line_start_point)
+
+        return total_region
+
+    def run(self, edit):
+        sel = self.view.sel()
+
+        new_regions = [self.expand_by_line(region) for region in sel]
+
+        # Don't add to the stack if the selection didn't change.
+        # pylint: disable=E0701
+        if all(old == new for old, new in zip(sel, new_regions)): return
+        # pylint: enable=E0701
+
+        sel.clear()
+        for region in new_regions:
+            sel.add(region)
+
+
+class ContractSelectionToLineAfter(sublime_plugin.TextCommand):
+    """Expands the current selection region by one line. On first call if the cursor is not at the beginning of the line, the current line will be selected.
+
+    Consecutive calls continue to expand the selection by another line.
+
+    """
+
+    def find_next(self, from_point, to_point):
+        """Walks backwards from a point, until it finds a region that matches
+        the given regex.
+
+        Attempts to find the longest match possible. So while "oobar:
+        function()" may be a match, this will continue walking backwards until
+        it can no longer match, since "foobar: function()" is preferred.
+
+        """
+
+        #find the current line number
+        (line, col) = self.view.rowcol(from_point)
+        end_point = self.view.line(to_point).end()
+
+        #find the point that corresponds to the beginnning of the line
+        line_point = self.view.text_point(line, 0)
+
+        #return if we reached the beginning
+        if not line_point > -1:
+            return None
+
+        if line_point == from_point:
+            #if we are at the beginning of the line the line has been selected already and we select theline before
+            start_point = self.view.text_point(line + 1, 0)
+        else:
+            start_point = line_point
+
+        return start_point, end_point
+
+    def contract_by_line(self, original_region):
+        """Returns a new, expanded region to cover the closest function that encloses the given region.
+
+        If there is no enclosing region, the original region is returned.
+
+        """
+        search_back_from_point = original_region.begin()
+        end_point = original_region.end()
+
+        line_start_point, end_point = self.find_next(search_back_from_point, end_point)
+
+        total_region = sublime.Region(line_start_point, end_point)
+        #scroll the view to show the selection
+        self.view.show(line_start_point)
+
+        return total_region
+
+    def run(self, edit):
+        sel = self.view.sel()
+
+        new_regions = [self.contract_by_line(region) for region in sel]
+
+        # Don't add to the stack if the selection didn't change.
+        # pylint: disable=E0701
+        if all(old == new for old, new in zip(sel, new_regions)): return
+        # pylint: enable=E0701
+
+        sel.clear()
+        for region in new_regions:
+            sel.add(region)
